2b93dc3a7d5c95f34814bab6770453350b248bc9
SAN <115963817+suryadinugroho@users.noreply.github.com>
2022-11-17 15:38:36 +0700

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -1,19 +1,10 @@
 function daftar(){
-
   var name = document.getElementById('name').value;
-  
   var email = document.getElementById('email').value;
-  
   var alamat = document.getElementById('alamat').value;
-
   if ( name == "" && email == "" && alamat == "") {
-  
         alert('silakan isi dengan lengkap')
-        
     }else{
-    
-        alert('informasi sukses')
-        
-    }
-    
+        alert('informasi sukses')   
+    } 
 }
