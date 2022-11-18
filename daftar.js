8d2da39a788f236655c21b78cffc6ff18104e6be
SAN <115963817+suryadinugroho@users.noreply.github.com>
2022-11-18 16:54:15 +0700

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -1,15 +1,21 @@
 function daftar(){
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var alamat = document.getElementById('alamat').value;
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var alamat = document.getElementById('alamat').value;
   
-  if ( name == "") {
-        alert('silakan isi dengan lengkap')
-    }else if( email == ''){
-        alert('silakan isi dengan lengkap')
-    }else if (alamat == '') {
-        alert('silakan isi dengan lengkap')
-    }else{
-        alert('sukses')
-    }
-}
+    if ( name == '') {
+          alert('silakan isi dengan lengkap')
+      }else if( email == ''){
+          alert('silakan isi dengan lengkap')
+      }else if (alamat == '') {
+          alert('silakan isi dengan lengkap')
+      }
+       
+       else if (name.length > 20){
+          alert('nama lengkap tidak boleh lebih 20 kata')
+      }else if( alamat.length > 100){
+          alert('alamat tidak boleh lebih dari 100 kata')
+      }else{
+          alert('sukses')
+      }
+   }
